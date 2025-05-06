05562716699def8e346b5383b7feaea09b081f06
HJMsmx2 <grupo1smx2c@gmail.com>
2025-05-06 16:54:52 +0200

    Update 1. Script_dinam.sh

--- 1. Script_dinam.sh
+++ 1. Script_dinam.sh
@@ -31,16 +31,16 @@ network:
     enp1s0:
       dhcp4: false
       addresses:
-        - $SVIP1/22
-    enp2s0:
-      dhcp4: false
-      addresses:
-        - $SVIP2/24
+        - 192.168.237.2/22
       routes:
         - to: default
           via: 192.168.236.1
       nameservers:
-        addresses: [192.168.236.1]
+        addresses: [8.8.8.8, 1.1.1.1]
+    enp2s0:
+      dhcp4: false
+      addresses:
+        - 192.168.1.2/24
 EOF_NETPLAN
 )
 
