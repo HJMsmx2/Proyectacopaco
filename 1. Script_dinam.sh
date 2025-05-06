7a2a22e2284aa6e8df630931b4337e23eb0b890f
HJMsmx2 <grupo1smx2c@gmail.com>
2025-05-06 16:19:26 +0200

    Update 1. Script_dinam.sh
    Superdinamismo

--- 1. Script_dinam.sh
+++ 1. Script_dinam.sh
@@ -5,6 +5,57 @@
 USERsv1="usuario"
 PASSsv1="usuario"
 PASSsv2="melon"
+SVIP0=$1                   # IP inicial de la Server
+SVIP1=192.168.237.2        # IP para Enp1s0 del Server
+SVIP2=192.168.1.2          # IP estática que se aplicará en la Enp2s0 del Server
+
+# ------------------------------------------------------------------
+# Parte local
+# ------------------------------------------------------------------
+
+echo "[+] Actualizando repositorios localmente..."
+echo "$PASSsv1" | sudo -S apt update
+#echo "$PASSsv1" | sudo -S apt upgrade -y
+
+echo "[+] Instalando sshpass en la máquina local..."
+echo "$PASSsv1" | sudo -S apt install sshpass -y
+
+echo "[+] Instalando ansible en la máquina local..."
+echo "$PASSsv1" | sudo -S apt install ansible -y
+
+# ------------------------------------------------------------------
+# Parte remota (Server)
+# ------------------------------------------------------------------
+
+echo "[+] Conectando y configurando la máquina Server..."
+
+sshpass -p "$PASSsv1" ssh -o StrictHostKeyChecking=no "$USERsv1@$SVIP0" bash << EOF
+echo "$PASSsv1" | sudo -S bash -c '
+    echo "[+] Cambiando contraseña de root..."
+    echo "root:$PASSsv2" | chpasswd
+
+    echo "[+] Cambiando hostname a Server..."
+    hostnamectl set-hostname Server
+
+    echo "[+] Habilitando acceso root por SSH..."
+    sed -i "s/^#\\?PermitRootLogin .*/PermitRootLogin yes/" /etc/ssh/sshd_config
+    systemctl restart ssh
+
+   if [ ! -f /etc/netplan/50-cloud-init.yaml.bkup ]; then
+    echo "[+] Haciendo copia de seguridad de Netplan..."
+    cp /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bkup
+   else
+    echo "[i] Backup de Netplan ya existe, no se sobrescribe."
+   fi
+
+    echo "[+] Reescribiendo Netplan..."
+    cat > /etc/netplan/50-cloud-init.yaml << EONET#!/bin/bash
+# Script de configuración desde la máquina local hacia la máquina Server
+
+# Variables
+USERsv1="usuario"
+PASSsv1="usuario"
+PASSsv2="melon"
 HOST_IP0=192.168.237.2         # IP actual de la Server
 HOST_IP=192.168.1.2          # IP estática que se aplicará
 
@@ -61,6 +112,19 @@ network:
               - $HOST_IP/24    
             routes:
               - to: default
+
+network:
+    ethernets:
+        enp1s0:
+            dhcp4: false
+            addresses:
+              - $SVIP1/22
+        enp2s0:
+            dhcp4: false
+            addresses:
+              - $SVIP2/24    
+            routes:
+              - to: default
                 via: 192.168.236.1
             nameservers:
               addresses: [192.168.236.1]
@@ -83,17 +147,17 @@ echo "[✔] Script ejecutado correctamente. La máquina Server ya está configur
 # ------------------------------------------------------------------
 echo "Creando clave publica"
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
-sshpass -p "$PASSsv2" ssh-copy-id root@"$HOST_IP0"
+sshpass -p "$PASSsv2" ssh-copy-id root@"$SVIP1"
 
 
     
 echo "[...] Esperando a que la máquina Server esté disponible en su nueva IP..."
 
 for i in {1..10}; do
-    ping -c 1 "$HOST_IP0" > /dev/null 2>&1 && break
-    echo "Esperando... ($HOST_IP0)"
+    ping -c 1 "$SVIP1" > /dev/null 2>&1 && break
+    echo "Esperando... ($SVIP1)"
     sleep 3
 done
 
 echo "[+] Comprobando conexión SSH como root..."
-ssh root@"$HOST_IP0"
+ssh root@"$SVIP1"
